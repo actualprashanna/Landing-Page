c47ffe9d8fabbb36544bb0a7ff5575c37f0efcc9
Prashanna Nepal <nepalprashanna34@gmail.com>
2020-06-18 09:04:35 +0545

    finished the about section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,35 @@
 const nayawala_checkbox = document.getElementById("nayawala");
 const navbar = document.querySelector('.navbar');
+const navbar_items = document.getElementsByClassName('navbar_link');
+const sections = document.getElementsByClassName('section');
+
 nayawala_checkbox.addEventListener('change', function () {
 
-navbar.classList.toggle('active');
+    navbar.classList.toggle('active');
 });
+
+function menuColorChange() {
+    for (let section of sections) {
+        let sb = section.getBoundingClientRect();
+
+        console.log(sb);
+        for (let item of navbar_items) {
+            let ib = item.getBoundingClientRect();
+            if (ib.y >= sb.y && ib.y + ib.height <= sb.y + sb.height) {
+                let color = section.dataset.color;
+                switch (color) {
+                    case 'light':
+                        item.classList.add('light');
+                        break;
+
+                    case 'dark':
+                        item.classList.remove('light');
+                        break;
+                }
+            }
+        }
+
+    }
+
+}
+document.addEventListener('scroll', menuColorChange);
